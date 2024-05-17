252783b7ea33153b74fcd2eaf84496f131548b3f
Brian Lister <brian@sparkforge.dev>
2024-05-17 17:26:45 -0500

    chore: delete app directory

--- app/bot-components/gateway-events/ready.ts
+++
@@ -1,9 +0,0 @@
-import { Events, type Client } from 'discord.js';
-
-export const readyEvent: GatewayEventHandler = {
-	type: Events.ClientReady,
-	once: true,
-	execute(client: Client) {
-		client.logger.info(`🟢 ${client.user?.tag} logged in.`);
-	},
-};
--- app/bot-components/scheduled-events/sample.ts
+++
@@ -1,8 +0,0 @@
-export const sampleEvent: ScheduledEvent = {
-	name: 'sample',
-	schedule: ['*/10 * * * *'],
-	timezone: 'Asia/Hong_Kong',
-	async execute(client) {
-		client.logger.debug('------------Tick------------');
-	},
-};
